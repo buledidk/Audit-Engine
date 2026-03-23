0091569d07b76b2abbcac041f15a56141b1c87e4
DK BULEDI <buledidk@gmail.com>
2026-03-23 13:01:58 +0000

    fix: point main.jsx entry to v10 App with RouterProvider

--- main.jsx
+++ main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AuditEngine from './src/AuditEngine.jsx';
+import App from './App.jsx';
+import './src/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuditEngine />
+    <App />
   </React.StrictMode>,
 );
